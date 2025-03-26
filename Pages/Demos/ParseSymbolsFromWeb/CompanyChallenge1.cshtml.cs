2a768fdca47579e90aed112cecaa87e1c0224b98
James <jsreed92@gmail.com>
2025-03-26 08:49:22 -0400

    Improves About Me page and adds Programming Challenge 1 page

---
+++ Pages/Demos/ParseSymbolsFromWeb/CompanyChallenge1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalWebsite.Pages.Demos.ParseSymbolsFromWeb
+{
+    public class CompanyChallenge1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
