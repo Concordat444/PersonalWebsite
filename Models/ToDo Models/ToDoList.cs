c571d5676b0eb4128a41df97314dde3c2c0d0a55
James <jsreed92@gmail.com>
2025-03-29 16:35:55 -0400

    Adds ToDo List and link to Azure Hosted Site.

---
+++ Models/ToDo Models/ToDoList.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonalWebsite.Models.ToDo_Models
+{
+    public class ToDoList
+    {
+        public List<ToDoModel> ToDos { get; set; } = [];
+        private int _NextId = 0;
+
+        public void AddToDo(string Item)
+        {
+            ToDos.Add(new ToDoModel(_NextId, Item));
+            _NextId++;
+        }
+        public void RemoveToDo(int id)
+        {
+            ToDos.Remove(ToDos.First(x => x.Id == id));
+        }
+        public void ToggleComplete(int id)
+        {
+            ToDos.First(x  => x.Id == id).ToggleComplete();
+        }
+    }
+}
---
+++ Models/ToDo Models/ToDoModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PersonalWebsite.Models.ToDo_Models
+{
+    public class ToDoModel
+    {
+        public int Id { get; set; }
+        public string Item { get; set; } = string.Empty;
+        public bool Complete { get; set; } = false;
+
+        public ToDoModel(int id, string task)
+        {
+            Id = id;
+            Item = task;
+        }
+
+        public void ToggleComplete()
+        {
+            Complete = !Complete;
+        }
+    }
+}
