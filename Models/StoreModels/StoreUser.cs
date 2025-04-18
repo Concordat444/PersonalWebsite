1520aa80a2e5f4170bd1b87086d1024fe2150319
James <jsreed92@gmail.com>
2025-04-18 10:46:33 -0400

    Adds incomplete StoreUser Class and reference to Testing Project.

---
+++ Models/StoreModels/StoreUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalWebsite.Models.StoreModels
+{
+    public class StoreUser
+    {
+        public long StoreUserId { get; set; }
+        [RegularExpression("^[a - zA - Z0 - 9_.-]{3, 20}$")]
+        public required string UserName { get; set; }
+    }
+}
