33a6ffd66c6ef4a807f422cc8a8b2620b42f740f
James <jsreed92@gmail.com>
2025-05-07 06:50:42 -0400

    Adds titles to Admin pages. Adds navigation component for admin pages.

---
+++ Components/Store/AdminNavMenuViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalWebsite.Components.Store
+{
+    public class AdminNavMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedRoute = HttpContext.GetRouteValue("Controller");
+            return View();
+        }
+    }
+}
